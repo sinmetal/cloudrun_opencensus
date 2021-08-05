9acaffbf7bdfe07416fdc924cd74e9ffe51d1f07
sinmetal <sinmetal@sinmetalcraft.jp>
2021-08-05 16:54:11 +0900

    jobを消した後、一応getしてみた

--- schedule_job_delete_handler.go
+++ schedule_job_delete_handler.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/vvakame/sdlog/aelog"
+	"google.golang.org/api/googleapi"
 	schedulerpb "google.golang.org/genproto/googleapis/cloud/scheduler/v1"
 )
 
@@ -24,12 +26,23 @@ func (h *Handlers) scheduleJobDeleteHandlerInternal(ctx context.Context, w http.
 	jobName := r.FormValue("jobName") // `projects/PROJECT_ID/locations/LOCATION_ID/jobs/JOB_ID`. を期待している
 	SetAttributesKV(ctx, map[string]interface{}{"jobNameParam": jobName})
 	if err := h.scheduler.DeleteJob(ctx, &schedulerpb.DeleteJobRequest{
-		Name: fmt.Sprintf(jobName),
+		Name: jobName,
 	}); err != nil {
 		fmt.Printf("failed Scheduler Job Delete:%s : %s\n", jobName, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return err
 	}
+
+	_, err = h.scheduler.GetJob(ctx, &schedulerpb.GetJobRequest{
+		Name: jobName,
+	})
+	gerr := &googleapi.Error{}
+	if errors.As(err, &gerr) && gerr.Code == http.StatusNotFound {
+		// ちゃんと消えてる
+	} else {
+		fmt.Printf("Schedule Job Get %s", err)
+	}
+
 	time.Sleep(5 * time.Minute) // すやすや
 	w.WriteHeader(http.StatusOK)
 	return nil
